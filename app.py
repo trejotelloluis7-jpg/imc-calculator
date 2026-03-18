b89483f391bd0e012176fac0c5b580d185a22893
Luis <trejotelloluis7@gmail.com>
2026-03-17 21:39:30 -0500

    web imc- python - flask

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("incex.html")
+
+
+@app.route("/calcular", methods=["POST"])
+def calcular():
+    altura = float(request.form.get("altura"))
+    peso = float(request.form.get("peso"))
+
+    IMC = peso / (altura ** 2)
+    if IMC >= 40:
+        resultado = "Tienes obesidad tipo III"
+        color = "red"
+    elif IMC >= 35:
+        resultado = "Tienes obesidad tipo II"
+        color = "orange"
+    elif IMC >= 30:
+        resultado = "Tienes obesidad tipo I"
+        color = "orange"
+    elif IMC >= 25:
+        resultado = "Estas en sobrepeso"
+        color = "gold"
+    elif IMC >= 22:
+        resultado = "Estas en el peso correcto"
+        color = "verde"
+    elif IMC >= 18.5:
+        resultado = "Estas en bajo peso"
+        color = "blue"
+    else:
+        resultado = "ESTAS EN DESNUTRICION"
+        color = "black"
+
+    return render_template("incex.html", imc=round(IMC, 2), resultado=resultado)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
